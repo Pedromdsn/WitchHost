b6f6039dfa71cb180340be7a55bc9c8bd99b0ccb
pedromdsn <pedromdsn.2002@hotmail.com>
2021-10-30 00:11:16 +0100

    fix: responsive

--- src/components/mine/SellZone.tsx
+++ src/components/mine/SellZone.tsx
@@ -3,7 +3,7 @@ interface MinecraftSellZoneProps {
 }
 
 const MinecraftSellZone = ({ children }: MinecraftSellZoneProps) => {
-	return <div className="px-20 max-w-[1500px] flex flex-wrap gap-6 mx-auto justify-center">{children}</div>
+	return <div className="px-20 max-w-[1300px] flex flex-wrap gap-6 mx-auto justify-center">{children}</div>
 }
 
 export default MinecraftSellZone
