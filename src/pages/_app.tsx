1d4ad3619e67be5891ed5c2aad8a42f7850e5022
Pedromdsn <pedromdsn.2002@hotmail.com>
2021-11-03 16:02:54 +0000

    new: display block font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
 				<link rel="icon" type="image/icon" sizes="16x16" href="/favicon.ico" />
 				<meta name="description" content="Minecraft host a um preço super gostoso." />
 				<meta name="author" content="Coco Blanco" />
-				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=block" rel="stylesheet" />
 			</Header>
 			<div className="bg-[#222230] font-roboto">
 				<Component {...pageProps} />
