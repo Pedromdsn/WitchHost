6f18f110b41aa37c14be4460ec5a71559135d004
pedromdsn <pedromdsn.2002@hotmail.com>
2022-01-07 14:34:56 +0000

    fix: footBar gamb

--- src/components/FootBar.tsx
+++ src/components/FootBar.tsx
@@ -6,9 +6,9 @@ const FootBar = () => {
 		<footer className="flex flex-col justify-center items-center pt-20 pb-10 text-white text-center">
 			<h3>Copyright © WitchHost | Todos direitos reservados</h3>
 			<h4 className="flex flex-wrap justify-center items-center">
-				{Dev} Desenvolvido por&#160;
+				{Dev} Desenvolvido por 
 				<Link href="https://pedromdsn.com" passHref>
-					<a className="font-bold">Coco Blanco</a>
+					<a className="font-bold ml-[0.5ch]">Coco Blanco</a>
 				</Link>
 			</h4>
 		</footer>
