c9cf51e1f52acc44a18679c075dc0e16650a3611
Pedro Nogueira <pedromdsn@hotmail.com>
2022-01-10 00:04:49 +0000

    fit: next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
 	swcMinify: true,
+  images: {
+		formats: ["image/avif", "image/webp"],
+	},
+  amp: true,
 	async redirects() {
     return [
       {
