5106bd63e3cf066de015cacf7f7a2ed2879adca1
Pedro Nogueira <pedromdsn.2002@hotmail.com>
2022-04-26 23:40:32 +0100

    fix: :zap: _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Header from "next/head"
+import Head from "next/head"
 
 import "tailwindcss/tailwind.css"
 import "../style/index.css"
@@ -6,13 +6,9 @@ import "../style/index.css"
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<Header>
+			<Head>
 				<title>A melhor hospedagem é aqui!</title>
-				<link rel="icon" type="image/icon" sizes="16x16" href="/favicon.ico" />
-				<meta name="description" content="Minecraft host a um preço super gostoso." />
-				<meta name="author" content="Pedromdsn" />
-				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=block" rel="stylesheet" />
-			</Header>
+			</Head>
 			<div className="bg-[#222230] font-roboto">
 				<Component {...pageProps} />
 			</div>
---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" type="image/icon" sizes="16x16" href="/favicon.ico" />
+          <meta name="description" content="Minecraft host a um preço super gostoso." />
+          <meta name="author" content="Pedromdsn" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=block" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
