34cd8be1194393c052ffb845f43abfe30edd3c15
Pedro Nogueira <pedromdsn.2002@hotmail.com>
2022-04-26 15:37:39 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
 				<title>A melhor hospedagem é aqui!</title>
 				<link rel="icon" type="image/icon" sizes="16x16" href="/favicon.ico" />
 				<meta name="description" content="Minecraft host a um preço super gostoso." />
-				<meta name="author" content="Coco Blanco" />
+				<meta name="author" content="Pedromdsn" />
 				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=block" rel="stylesheet" />
 			</Header>
 			<div className="bg-[#222230] font-roboto">
